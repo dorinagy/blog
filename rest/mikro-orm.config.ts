b4af8735d577caac3f5d38d0ef0d2682e6c390df
Nagy Dóra <nagy.dora@dotosystems.hu>
2022-02-20 16:45:12 +0100

    modify config

--- rest/mikro-orm.config.ts
+++ rest/mikro-orm.config.ts
@@ -1,9 +1,11 @@
 import { IDatabaseDriver, Options } from '@mikro-orm/core';
+import { Category } from './src/categories/entities/category';
 import { BlogPost } from './src/posts/entities/blogpost';
+import { Comment } from './src/posts/entities/comment';
 import { User } from './src/users/entities/user';
 
 export default {
-  entities: [BlogPost, User],
+  entities: [BlogPost, Category, Comment, User],
   dbName: (process.env.seed ? './dist/' : '') + process.env.dbName,
   type: 'sqlite',
   migrations: {
